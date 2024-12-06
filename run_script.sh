70a51dd250ef3e14a65f6b81e08f98b3a7a39e1b
Divy Raj Saini <143434285+Dead-Bytes@users.noreply.github.com>
2024-12-06 12:13:06 +0530

    added run_script.sh

---
+++ run_script.sh
@@ -0,0 +1,32 @@
+## clone the repo
+
+
+git clone https://github.com/Dead-Bytes/LLaMA-Factory.git
+
+
+## install the reqirements
+
+cd LLaMA-Factory
+pip install -r requirements.txt
+
+
+## move the datasets 
+## https://drive.google.com/file/d/1w2evsNwFBJ61epzNoWFyhsnruPMYzUMj/view?usp=drive_link
+
+gdown https://drive.google.com/uc?id=1w2evsNwFBJ61epzNoWFyhsnruPMYzUMj
+
+mv  solidity_final_train.json data/
+
+
+## install llama_facrtory
+
+
+pip install -e .
+## run the finetune sccripts.
+
+llamafactory-cli train my_scripts/qwen_3B_sol4/finetune.yaml
+
+## run the export script.
+
+llamafactory-cli merge my_scripts/qwen_3B_sol4/merge.yaml
+
